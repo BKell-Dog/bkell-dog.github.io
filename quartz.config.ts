7819636867e0b00792168592837d9fe9a5551ad8
BKell-Dog <bkelldog59@gmail.com>
2026-03-28 00:03:05 -0400

    Update quartz config.

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Wave Lab Knowledge",
+    pageTitle: "WK Knowledge Base",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "wavelab.neocities.org",
+    baseUrl: "willkelly.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
