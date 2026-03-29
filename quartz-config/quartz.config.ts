3558d8bcb854428b9b0d14ea35ceeba048339e72
BKell-Dog <bkelldog59@gmail.com>
2026-03-29 00:41:40 -0400

    Attempt fix SCSS issue.

--- quartz-config/quartz.config.ts
+++ quartz-config/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "willkelly.dev",
+    baseUrl: "willkelly.dev/knowledge",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
